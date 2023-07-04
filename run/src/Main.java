bf65f6090d957c66547a68f7512337cb56fd21f1
Ann S <404region@gmail.com>
2023-07-04 17:16:01 +0300

    Add Exceptions

--- run/src/Main.java
+++ run/src/Main.java
@@ -8,14 +8,19 @@ import java.util.regex.Pattern;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // Press Opt+Enter with your caret at the highlighted text to see how
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите математическое выражение: ");
+        System.out.print("Введите математическое выражение в одну строку: ");
         String calcStringIn = in.nextLine();
-        System.out.print("calcStringIn ");
-        System.out.print(calcStringIn);
-        String result = calc(calcStringIn);
+        String result = "";
+        try {
+            result = calc(calcStringIn);
+        } catch (Exception e) {
+            System.out.println("Ошибка при вычислении выражения: " + e.getMessage());
+        }
+
+        System.out.print(result);
     }
     static String eval(String a, String operator, String b) {
         int r = 0;
@@ -38,7 +43,7 @@ public class Main {
         return Integer.toString(r);
     }
 
-    static String replaceRomanNumber(String romanNumber) {
+    static String replaceRomanNumber(String romanNumber) throws Exception {
         String r = "1";
 
         switch (romanNumber) {
@@ -74,12 +79,12 @@ public class Main {
                 break;
             default:
                 //  выкинуть ошибку
-                break;
+                throw new Exception("В справочнике нет нужного числа");
         }
 
         return r;
     }
-    static String replaceArabicNumberToRoman(String arabicNumber) {
+    static String replaceArabicNumberToRoman(String arabicNumber) throws Exception {
         String r = "I";
 
         switch (arabicNumber) {
@@ -145,7 +150,7 @@ public class Main {
                 break;
             default:
                 //  выкинуть ошибку
-                break;
+                throw new Exception("В справочнике нет нужного числа");
         }
 
         return r;
@@ -155,86 +160,66 @@ public class Main {
         String patternStr = "I|V|X";
         return Pattern.compile(patternStr).matcher(str).find();
     }
-    public static String calc(String input) {
-        System.out.print("calc function ");
+    public static String calc(String input) throws Exception {
         // Надо выкинуть из строки пробелы
         String formatInput = input.replaceAll(" ", "");
-        System.out.print("formatInput ");
-        System.out.print(formatInput);
 
-        // Проверить что нету римских чисел
+        // Проверить на римские чисел
         if(checkForRoman(formatInput)) {
             // итак римские цифры есть
             // получить операнды и оператор для римских чисел
-            String patternRomanStr = "([I|V|X]+)([^I|V|X])([I|V|X]+)";
+            String patternRomanStr = "^([I|V|X]+)([^I|V|X])([I|V|X]+)$";
             Pattern patternRoman = Pattern.compile(patternRomanStr);
             Matcher matcher = patternRoman.matcher(formatInput);
 
             if (matcher.find())
             {
-                String operand1 = matcher.group(1);
-                String operator = matcher.group(2);
-                String operand3 = matcher.group(3);
-                System.out.println("group");
-                System.out.println(matcher.group(1));
-                System.out.println(matcher.group(2));
-                System.out.println(matcher.group(3));
+                try {
+                    String operand1 = matcher.group(1);
+                    String operator = matcher.group(2);
+                    String operand3 = matcher.group(3);
 
-                String resultString = eval(replaceRomanNumber(operand1), operator, replaceRomanNumber(operand3));
+                    String resultString = eval(replaceRomanNumber(operand1), operator, replaceRomanNumber(operand3));
 
-                // сделать мат операцию
-                System.out.println(replaceArabicNumberToRoman(resultString));
-                return replaceArabicNumberToRoman(resultString);
+                    // сделать мат операцию
+                    return replaceArabicNumberToRoman(resultString);
+                } catch (Exception e) {
+                    System.out.println("Ошибка при вычислении выражения: " + e.getMessage());
+                }
             } else {
-                // выбросить исключение?
+                throw new Exception("Оба операнда должны быть в одной системе исчисления  и количество операндов должно быть строго 2");
+            }
+        } else {
+            // получить операнды и оператор для арабских чисел
+            String patternStr = "^(\\d+)(\\D)(\\d+)$";
+            Pattern pattern = Pattern.compile(patternStr);
+            Matcher matcher = pattern.matcher(formatInput);
+
+            /*String patternErrorStr = "(\\d+)(\\D)(\\d+)(\\D)";
+            Pattern patternError = Pattern.compile(patternStr);
+            Matcher matcherError = pattern.matcher(formatInput);
+            if (matcherError.find()) {
+                throw new Exception("Оба операнда должны быть в одной системе исчисления и количество операндов должно быть строго 2");
+            }*/
+
+            if (matcher.find())
+            {
+                try {
+                    String operand1 = matcher.group(1);
+                    String operator = matcher.group(2);
+                    String operand3 = matcher.group(3);
+                    // сделать мат операцию
+                    String resultString = eval(operand1, operator, operand3);
+                    return resultString;
+                } catch (Exception e) {
+                    System.out.println("Ошибка при вычислении выражения: " + e.getMessage());
+                }
+            } else {
+                throw new Exception("Оба операнда должны быть в одной системе исчисления и количество операндов должно быть строго 2");
             }
         }
 
-
-        // получить операнды и оператор для арабских чисел
-        String patternStr = "(\\d+)(\\D)(\\d+)";
-        Pattern pattern = Pattern.compile(patternStr);
-        Matcher matcher = pattern.matcher(formatInput);
-
-        if (matcher.find())
-        {
-            String operand1 = matcher.group(1);
-            String operator = matcher.group(2);
-            String operand3 = matcher.group(3);
-            System.out.println("group");
-            System.out.println(matcher.group(1));
-            System.out.println(matcher.group(2));
-            System.out.println(matcher.group(3));
-
-            String resultString = eval(operand1, operator, operand3);
-
-            // сделать мат операцию
-            System.out.println(resultString);
-            return resultString;
-        } else {
-            // выбросить исключение?
-        }
-
-        // выкинуть исключения
-
         return input;
-    }
-
-}
-
-class Info{
-    int myInt;
-    void speak() {
-        System.out.println(myInt);
-    }
-
-    void forAll() {
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
     }
 
 }
